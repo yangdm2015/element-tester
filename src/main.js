0950e7badb496a7e3fbdd5e8b73ca28aaeb9964d
杨杉 <yangshan08@meituan.com>
2018-08-12 11:00:20 +0800

    update
    swimlane=MTCS-9492

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
+import router from "./router.js"
+
 
 Vue.use(ElementUI)
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+    el: '#app',
+    router,
+    render: h => h(App)
 })

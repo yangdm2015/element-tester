04f965c35cf20f7e5ae2f6668807b5b750a74644
yangshan08 <yangshan08@meituan.com>
2018-10-28 12:19:35 +0800

    no message

--- src/components/keyTest/index.vue
+++ src/components/keyTest/index.vue
@@ -6,13 +6,16 @@
                               :name="1">
                 <el-row>
                     <el-col :span="8">
-                        <div v-html="code1"></div>
-                        <div>
-                            tag1List =
-                        </div>
-                        <div>
-                            {{tag1List}}
-                        </div>
+                        <!-- <info></info> -->
+                        <!-- <article v-html="code1"></article> -->
+                        <mavon-editor :value="code1"
+                                      :editable="false"
+                                      :ishljs="true"
+                                      :toolbarsFlag="false"
+                                      :subfield="false" />
+
+                        <!-- <div v-html="tmp">
+                        </div> -->
                     </el-col>
                     <el-col :span="16">
                         <el-button @click="shift1">
@@ -28,7 +31,7 @@
                     </el-col>
                 </el-row>
             </el-collapse-item>
-            <el-collapse-item title=":key = index+tag.content   时，list内的元素的key值会保持唯一"
+            <el-collapse-item title=":key = index+tag.content+tag.type   时，list内的元素的key值会保持唯一"
                               :name="2">
                 <el-row>
                     <el-col :span="8">
@@ -63,12 +66,15 @@
 </template>
 
 <script>
+import marked from 'marked';
+import info from '../../md/info.md'; // 导入md文件
 import customTag from './customTag.vue';
 export default {
     data() {
         return {
             tag1List: [
                 { type: '', content: '默认' },
+
                 { type: 'success', content: '成功' },
                 { type: 'info', content: '灰色' },
                 { type: 'warning', content: '警告' },
@@ -82,17 +88,28 @@ export default {
                 { type: 'danger', content: '错误' }
             ],
             activeNames: [1, 2, 3, 4],
-            code1: `  <custom-tag v-for="(tag,index) in tag1List"
-                        :type="tag.type"
-                        :content="tag.content"
-                        :key="index"></custom-tag>`,
             code2: `  <custom-tag v-for="(tag,index) in tag1List"
                         :type="tag.type"
                         :content="tag.content"
-                        :key="index+content"></custom-tag>`
+                        :key="index+tag.content+tag.type"></custom-tag>`,
+            tmp: `  <custom-tag v-for="(tag,index) in tag1List"
+                        :type="tag.type"
+                        :content="tag.content"
+                        :key="index"></custom-tag>`
         };
     },
-
+    computed: {
+        code1() {
+            let tmp = `  <custom-tag v-for="(tag,index) in tag1List"
+                        :type="tag.type"
+                        :content="tag.content"
+                        :key="index"></custom-tag>`;
+            return (
+                tmp +
+                `\n\ntag1List = ${this.tag1List.map(i => JSON.stringify(i))}`
+            );
+        }
+    },
     methods: {
         shift1() {
             this.tag1List.shift();
@@ -115,7 +132,40 @@ export default {
         handleChange() {}
     },
     components: {
-        customTag
+        customTag,
+        info
     }
 };
 </script>
+
+
+<style lang="scss" scoped>
+@import './color-brewer.css'; //导入高亮样式
+.hello {
+    margin: 20px auto;
+    width: 50%;
+}
+a {
+    color: #409eff;
+    text-decoration: none;
+}
+
+code {
+    background-color: #f9fafc;
+    padding: 0 4px;
+    border: 1px solid #eaeefb;
+    border-radius: 4px;
+}
+
+.hljs {
+    line-height: 1.8;
+    font-family: Menlo, Monaco, Consolas, Courier, monospace;
+    font-size: 12px;
+    padding: 18px 24px;
+    background-color: #fafafa;
+    border: solid 1px #eaeefb;
+    margin-bottom: 25px;
+    border-radius: 4px;
+    -webkit-font-smoothing: auto;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -4,9 +4,18 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 import router from "./router.js"
-
-
+import mavonEditor from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
+// import hljs from 'highlight.js';
+// import 'highlight.js/styles/googlecode.css' //样式文件
+// Vue.directive('highlight', function (el) {
+//     let blocks = el.querySelectorAll('pre code');
+//     blocks.forEach((block) => {
+//         hljs.highlightBlock(block)
+//     })
+// })
 Vue.use(ElementUI)
+Vue.use(mavonEditor)
 
 new Vue({
     el: '#app',
